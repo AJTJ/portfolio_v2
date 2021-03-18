ae880c0377a57e4ed4f9225adfbfd0f85830a958
AJTJ <janke.aaron@gmail.com>
2021-03-18 13:03:17 -0400

    Updates

---
+++ precache-manifest.84f3884a504a3481334a943c42b519bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b223d247b9d45b9a7e9fa1b2ddfe2b6b",
+    "url": "/portfolio_v2/index.html"
+  },
+  {
+    "revision": "fcad27e16f8ef9447917",
+    "url": "/portfolio_v2/static/js/2.c1f91e05.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio_v2/static/js/2.c1f91e05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc7ef9887d41cfbadb8e",
+    "url": "/portfolio_v2/static/js/main.574d2753.chunk.js"
+  },
+  {
+    "revision": "50e364d9cf87c6b57bee",
+    "url": "/portfolio_v2/static/js/runtime-main.2914acf5.js"
+  },
+  {
+    "revision": "2295159b4de60d16df32a211f33efe16",
+    "url": "/portfolio_v2/static/media/rust.2295159b.svg"
+  }
+]);
