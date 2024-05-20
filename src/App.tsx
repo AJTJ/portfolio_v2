f409a8fb62aed949f72c2fe04392e134cc57fd6f
AJTJ <janke.aaron@gmail.com>
2024-05-20 14:19:22 +0800

    update Mon 20 May 2024 14:19:22 WITA

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Portfolio from "./Portfolio";
 import { ThemeProvider } from "@emotion/react";
 import { theme, Global } from "./Portfolio/theme";
 
+//little update with new images
+
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
